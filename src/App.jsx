f7d87eca1de509e86396e7e00e1a92efbfed7a70
JAAGRIT02 <jaagrit08@gmail.com>
2024-05-02 12:52:15 +0530

    redoing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
-import Square from './components/square'
 
 function App() {
   return (
   <>
   <div className='card'>
       <h1>title</h1>
-      
-      <Square />
-    
   </div>
   </>
   )
