cf049a2e18ed25c61df4d3a7648968765a8c3b3b
JAAGRIT02 <jaagrit08@gmail.com>
2024-05-02 13:00:57 +0530

    made new component foursides

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Foursides from './components/Foursides'
 
 function App() {
   return (
   <>
   <div className='card'>
       <h1>title</h1>
+      <Foursides/>
   </div>
   </>
   )
---
+++ src/components/Foursides.jsx
@@ -0,0 +1,7 @@
+const Foursides = ()=>{
+    return <div>
+        <h1>hello</h1>
+    </div>
+}
+
+export default Foursides
