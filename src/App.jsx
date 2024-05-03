7480a776982f32b96c0fd0f2f2f0f1b0cdd79208
JAAGRIT02 <jaagrit08@gmail.com>
2024-05-03 14:50:57 +0530

    learning basic stuff

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ function App() {
   <>
   <div className='card'>
       <h1>title</h1>
-      <Foursides/>
+      <img src="vite.svg" alt="" />
+
+      <Foursides value ="1">
+      <div>hii</div>
+      <h3>you there</h3>
+      </Foursides>
+      <Foursides value ="2"/>
+      <Foursides value ="3"/>
+      <Foursides value ="4"/>
+      <Foursides value ="5"/>
   </div>
   </>
   )
--- src/components/Foursides.jsx
+++ src/components/Foursides.jsx
@@ -1,6 +1,10 @@
-const Foursides = ()=>{
-    return <div>
-        <h1>hello</h1>
+// eslint-disable-next-line react/prop-types
+const Foursides = ({ value ,children })=>{
+    console.log(children);
+    return <div>{value}
+
+    <h5>children will be rendered below</h5>
+    <div>{children}</div>
     </div>
 }
 
