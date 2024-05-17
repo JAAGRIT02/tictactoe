b6a5c10f5f582832e070fd0f2e63223374598870
JAAGRIT02 <jaagrit08@gmail.com>
2024-05-17 12:53:02 +0530

    lifted up functionality form board to app.jsx . added winner .js to calculate winner. added status message.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 import './components/styles.scss'
 import { useState } from 'react';
-import Board from './components/Board'
+import Board from './components/Board';
+import { calculateWinner } from './components/winner';
 
 
 function App() {
@@ -9,10 +10,17 @@ function App() {
   const[squares,setSquares]= useState(Array(9).fill(null));
   const[isXNext,setIsXNext]= useState(false);
 
-  console.log(squares);
+
+  const winner = calculateWinner(squares);
+  const nextPlayer = isXNext ? 'X':'O';
+  const statusMessage = winner ? `THE WINNER IS ${winner}` : `THE NEXT PLAYER IS ${nextPlayer}`;
+
+  console.log(winner);
+
+  // console.log(squares);
   const handleSquareClick = clickedPosition=>{
 
-    if(squares[clickedPosition]){
+    if(squares[clickedPosition] || winner){
         return;
     }
     
@@ -36,7 +44,7 @@ function App() {
   return (
   <>
   <div className='app'>
-    <h2>NEXT PLAYER IS SOMEBODY</h2>
+    <h2>{statusMessage}</h2>
     <Board squares = {squares} handleSquareClick = {handleSquareClick}/>
   </div>
   </>
